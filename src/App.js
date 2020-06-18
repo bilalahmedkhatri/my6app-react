be23bbf156389c442ead18eb7f2a24c0fce3131d
bilalahmedkhatri <bilalahmedkhatri@github.com>
2020-06-19 02:13:53 +0500

    Initialize project using Create React App
    First Init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Mode from './mode';
+
+function App() {
+  return (
+    <center>
+      <div class='main'>
+        <Mode />
+      </div>
+    </center>
+  );
+}
+export default App;
---
+++ src/mode.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function Mode () {
+    let [isday, setDay] = useState(true);
+    let [count, setCount] = useState(0);
+    return (
+        <div className={`${isday ? 'daymode' : 'nightmode'}`}>
+            <br/>
+            <span>
+                <p>This Number shows Increment and Decrement {count}</p>
+                <button onClick={()=> setCount(++count)}>Increment</button>
+                &nbsp;
+                <button onClick={()=> setCount(--count)}>Decrement</button>
+                <br/>
+            </span>
+            <br/>
+            <span>
+                This is <b>{isday ? 'DAY-MODE' : 'NIGHT-MODE'}</b>
+                <br/>
+                <br/>
+                <button onClick={()=> setDay(!isday)}>Day/Night Mode</button>
+                <br/>
+                <br/>
+            </span>
+        </div>
+    );
+}
+
+export default Mode;
